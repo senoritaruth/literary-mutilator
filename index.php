552520b374622820bffffba6b9ed3164510ef397
Ruth Cisneros <senoritaruth@gmail.com>
2019-08-08 17:24:57 -0600

    working on the first fn

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport"
+				content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Document</title>
+	</head>
+	<body>
+		<div class="prgh">
+		<div id="prgh-1">
+		<p id="zombie">Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.</p>
+		</div>
+		<div id="prgh-2">
+		<p id="hipster">Everyday carry air plant actually food truck, deserunt before they sold out eiusmod. Truffaut kitsch 3 wolf moon tote bag, austin duis paleo. +1 meh selfies butcher, tumblr farm-to-table gastropub 8-bit schlitz ramps street art waistcoat consequat. Live-edge et mumblecore non ad sint cred semiotics la croix kitsch artisan ugh vexillologist swag. Oh. You need a little dummy text for your mockup? How quaint. I bet you’re still using Bootstrap too…</p>
+		</div>
+		<div id="prgh-3">
+		<p id="cat">Cat ipsum dolor sit amet, somehow manage to catch a bird but have no idea what to do next, so play with it until it dies of shock for purr. Why use post when this sofa is here chase ball of string spread kitty litter all over house, but cry louder at reflection yet mouse yet meow and walk away i am the best. Chew master's slippers murf pratt ungow ungow curl up and sleep on the freshly laundered towels and kitty pounce, trip, faceplant you didn't see that no you didn't definitely didn't lick, lick, lick, and preen away the embarrassment for i shredded your linens for you. Shred all toilet paper and spread around the house. Stare at ceiling when owners are asleep, cry for no apparent reason bathe private parts with tongue then lick owner's face pelt around the house and up and down stairs chasing phantoms kitty poochy run up and down stairs. Have my breakfast spaghetti yarn rub face on everything. Catch mouse and gave it as a present human is washing you why halp oh the horror flee scratch hiss bite, bathe private parts with tongue then lick owner's face so litter kitter kitty litty little kitten big roar roar feed me i just saw other cats inside the house and nobody ask me before using my litter box yet give me attention or face the wrath of my claws for present belly, scratch hand when stroked. Where is my slave? I'm getting hungry. </p>
+		</div>
+		</div>
+	<script src="./lm.js"></script>
+	</body>
+</html>
---
+++ lm.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+	initLitMut();
+};
+
+function initLitMut () {
+	let prgh = document.getElementsByClassName("prgh");
+	let zhc = {
+		zombie: document.getElementById("zombie"),
+		hipster: document.getElementById("hipster"),
+		cat: document.getElementById("cat")
+	};
+
+.addEventListener('scroll', {
+	handleEvent(evt) {
+	}
+	})
+}
