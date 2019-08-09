9040fb0c4137345b5eda18869a000e971caccf17
Ruth Cisneros <senoritaruth@gmail.com>
2019-08-09 01:19:59 -0600

    mapped the scroll(bar) to delete the zombie paragraph.

--- lm.js
+++ lm.js
@@ -1,15 +1,12 @@
 window.onload = function() {
-	let zombie = document.getElementById("zombie");
-	zombie.addEventListener('click', function(event) {
-		deleteElement(event);
-	});
+	window.addEventListener('scroll', function(event) {
+		deleteFirstWord(event);
+	}, false);
 };
 
-function deleteElement (event) {
-	console.log(event.target.innerText);
-	event.target.innerText = event.target.innerText.replace(/^\w+\W*/, "");
-//	event.target.innerText.replace(/^\w+\W*/, "");
-
-
+function deleteFirstWord (event) {
+	console.log("test");
+	zombie = document.getElementById("zombie");
+	zombie.innerText = zombie.innerText.replace(/^\w+\W*/, "");
 };
 
