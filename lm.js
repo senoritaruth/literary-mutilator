e529d83c9b3ac7b8c69b7616730d2a0f2e7ab8b1
Ruth Cisneros <senoritaruth@gmail.com>
2019-08-09 02:02:54 -0600

    turned zombie text green bc that's funny, made the hipster magenta, and now figuring out rot 13.

--- lm.js
+++ lm.js
@@ -1,12 +1,34 @@
 window.onload = function() {
+	let zombie = document.getElementById("zombie");
+	zombie.addEventListener("click", function(event) {
+		changeColor(event, "Green");
+	});
+
 	window.addEventListener('scroll', function(event) {
 		deleteFirstWord(event);
-	}, false);
+	});
+
+	let hipster = document.getElementById("hipster");
+	hipster.addEventListener("click", function(event) {
+		changeColor(event, "Magenta");
+	});
+
+	let cat = document.getElementById("cat");
+	cat.addEventListener("click", function(event) {
+		rot13(event);
+	})
 };
 
-function deleteFirstWord (event) {
-	console.log("test");
-	zombie = document.getElementById("zombie");
+function deleteFirstWord(event) {
+	let zombie = document.getElementById("zombie");
 	zombie.innerText = zombie.innerText.replace(/^\w+\W*/, "");
 };
 
+function changeColor(event, color) {
+	event.target.style.color = color;
+};
+
+function rot13(event) {
+	event.target.innerText = str_rot13.event.target.innerText
+
+}
