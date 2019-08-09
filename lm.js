72a01dda15b39ce2c8c80c9844c8b4ffa806d408
Ruth Cisneros <senoritaruth@gmail.com>
2019-08-09 00:11:37 -0600

    got it to work with a click, but not a scroll yet. learned how to point the function to the right string.

--- lm.js
+++ lm.js
@@ -1,14 +1,15 @@
 window.onload = function() {
 	let zombie = document.getElementById("zombie");
-	zombie.addEventListener('scroll', function(event) {
+	zombie.addEventListener('click', function(event) {
 		deleteElement(event);
 	});
 };
 
 function deleteElement (event) {
-	event.target.innerText.replace(/^\w+\W*/, "");
+	console.log(event.target.innerText);
+	event.target.innerText = event.target.innerText.replace(/^\w+\W*/, "");
+//	event.target.innerText.replace(/^\w+\W*/, "");
+
+
 };
-
-
-
 
